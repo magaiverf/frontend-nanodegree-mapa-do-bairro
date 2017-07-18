e32645699ce40d7dc31a9d19f2ee2c6642df16c0
magaiverf <magaiverf@gmail.com>
2017-07-18 14:18:06 -0300

    Add the missing animation

--- map.js
+++ map.js
@@ -35,7 +35,14 @@ function initMap() {
     function populateInfoWindow(marker, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != marker) {         
-            getSuggestionBeard();               
+            getSuggestionBeard();     
+            
+            // set animations
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            window.setTimeout(function() {
+                marker.setAnimation(null);
+            }, 3000);
+
             // Clear the infowindow content to give the streetview time to load.
             infowindow.marker = marker;
             infowindow.setContent(`
